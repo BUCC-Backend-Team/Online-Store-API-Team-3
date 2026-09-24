bf32cc1bde2ca112f87da7d1ef6ec17fa8a85a28
Dan <dan@example.com>
2026-09-24 20:45:36 +0100

    Configure JWT authentication and User RBAC foundation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'admin@example.com',
+            'role' => 'ADMIN',
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Test Customer',
+            'email' => 'customer@example.com',
+            'role' => 'CUSTOMER',
         ]);
     }
 }
